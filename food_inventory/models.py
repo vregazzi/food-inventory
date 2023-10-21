698d21e8cddc76e450bf0635af8efcd8391178ec
Vincent Regazzi <vincentregazzi@gmail.com>
2023-10-20 22:24:14 -0500

    better database structure

--- food_inventory/models.py
+++ food_inventory/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
     calories = models.IntegerField()
-    protein = models.IntegerField()
     fat = models.IntegerField()
     carbohydrates = models.IntegerField()
+    protein = models.IntegerField()
     quantity = models.IntegerField()
 
     def get_nutrition(self) -> dict[str, int]:
@@ -20,15 +20,20 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
-    # field for ingredients and quantity
     ingredients = models.JSONField()
-    # ingredients = dict[str, int]
+    servings = models.IntegerField(default=0)
 
     def get_nutrition(self) -> dict[str, int]:
         nutrition = {
             'calories': 0,
             'protein': 0,
             'fat': 0,
-            'carbohydrates': 0
-        }
+            'carbohydrates': 0,
+            }
+        for ingredient in self.ingredients:
+            obj = Ingredient.objects.get(name=ingredient)
+            nutrition['calories'] += obj.calories
+            nutrition['protein'] += obj.protein
+            nutrition['fat'] += obj.fat
+            nutrition['carbohydrates'] += obj.carbohydrates
         return nutrition
